8d3100585b5c1f2a19e4a5381f1e91b77a0baf75
Eka Prasetia <ekaone3033@gmail.com>
2026-03-29 22:02:29 +0700

    removd mjs

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
+  format: ["cjs"],
+  outExtension: () => ({ js: ".js" }),
   banner: {
     js: "#!/usr/bin/env node",
   },
